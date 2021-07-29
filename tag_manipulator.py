51e77a20b53c7155df7ed7fc991f468e508d6389
unknown <bakhai.veera@gmail.com>
2021-07-29 16:30:13 +0100

    cut2 - second push

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -1,3 +1,4 @@
+import re
 class TagManipulator():    
     def parse_string(self, tags):
         result = []
@@ -6,4 +7,13 @@ class TagManipulator():
             return result
 
         return result
-        
+
+class TagManipulator():    
+    def parse_string(self, tags, regex=""):
+        result = []
+
+        tempResult = re.split( regex, tags )
+        if( len(tempResult[0]) > 0 ):
+            result = tempResult  
+
+        return result
--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -12,3 +12,18 @@ def test_split_empty_string_result_empty_array():
 
     # assert
     assert result == expResult
+
+
+def test_split_one_string_result_array_of_one():
+    # arrange
+    stringToSplit = "java"
+    regex = ","
+    expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
